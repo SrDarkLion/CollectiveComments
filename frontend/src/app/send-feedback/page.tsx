0c7dd07c3598f7ec2fa547ff1772c524a1b7d4b3
Sara Romao <sara-romao@live.com>
2025-03-02 22:20:07 -0300

    feat(frontend): enhance feedback page styling
    - Updated the feedback page layout to include a bordered container for better visual separation.
    - Changed the feedback prompt text to include a supportive message for users.
    - Adjusted text colors for improved readability and emphasis.

--- frontend/src/app/send-feedback/page.tsx
+++ frontend/src/app/send-feedback/page.tsx
@@ -5,8 +5,9 @@ export default function sendFeedback() {
   return (
     <div className="flex justify-center pt-5">
       <div className="mx-auto max-w-xl w-full px-4 sm:px-6 lg:px-8">
-        <p className="text-center text-lg font-medium mb-8">Enviar FeedBack</p>
-        <div className="mx-auto w-full">
+        <div className="mx-auto w-full border rounded-md p-4 sm:p-6 lg:p-8">
+          <p className="text-lg font-medium mb-8 text-indigo-600 flex flex-col">Envie o FeedBack <span className="text-black text-base font-normal">A empresa agradece seu feedback</span></p>
+         
           <SendFeedbackForm/>
         
         </div>
