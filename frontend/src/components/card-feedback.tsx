577fa7de47f3b973720309b4fefea3736edab115
Sara Romao <sara-romao@live.com>
2025-03-04 14:36:57 -0300

    feat(frontend): add CardFeedback component

---
+++ frontend/src/components/card-feedback.tsx
@@ -0,0 +1,11 @@
+
+
+export default function CardFeedback() {
+  return (
+    <div className="shadow-sm rounded-md bg-purple-100 p-5">
+      <h4 className="font-semibold text-lg">Tipo</h4>
+      <p className="text-sm font-medium">06 de fev</p>
+      <p className="">Supporting line text lorem ipsum dolor sit amet, consectetur.</p>
+    </div>
+  )
+}
